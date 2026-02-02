93b37b82fd4ef919fac84092a9e634b27109167c
JayanthReddyKonda <kondajayanthreddy@gmail.com>
2026-02-02 18:29:28 +0530

    Add Tailwind CSS, DaisyUI, and toast support

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App.jsx'
 import {BrowserRouter} from 'react-router'
+import {Toaster} from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Toaster />
+    <App />  
   </BrowserRouter>
 )
